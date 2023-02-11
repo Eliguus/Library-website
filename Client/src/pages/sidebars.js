ba69bef9afdb50be2111f53bb093490560b540c3
Easeandpearl <yusrayakut7589@gmail.com>
2023-02-11 15:40:29 +0300

    added sidebar

---
+++ Client/src/pages/sidebars.js
@@ -0,0 +1,9 @@
+/* global bootstrap: false */
+(function () {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
