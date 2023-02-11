2a967c4b78e57d28499e5a06bdb1d71291d49523
morphou <110639168+morphou@users.noreply.github.com>
2023-02-11 15:30:38 +0300

    Added Books endpoint

---
+++ Server/src/books/books.controller.ts
@@ -0,0 +1,210 @@
+import {
+  Controller,
+  Body,
+  Param,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Request,
+  HttpStatus,
+  HttpException,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { extname } from 'path';
+import { existsSync, mkdirSync } from 'fs';
+import { diskStorage } from 'multer';
+import { v4 as uuid } from 'uuid';
+// Service
+import { BooksService } from './books.service';
+import { AuthGuard } from '@nestjs/passport';
+// DTO
+import { BookDto } from './dto/book.dto';
+import { BookIdDto } from './dto/book-id.dto';
+import { AuthService } from 'src/users/user.auth.service';
+import { UsersService } from 'src/users/users.service';
+
+let newFileName = '';
+
+export const multerConfig = {
+  dest: './public/images',
+};
+
+export const multerOptions = {
+  limits: {
+    fileSize: 100023,
+  },
+  fileFilter: (req: any, file: any, cb: any) => {
+    if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+      cb(null, true);
+    } else {
+      cb(
+        new HttpException(
+          `Unsupported file type ${extname(file.originalname)}`,
+          HttpStatus.BAD_REQUEST,
+        ),
+        false,
+      );
+    }
+  },
+
+  storage: diskStorage({
+    destination: (req: any, file: any, cb: any) => {
+      const uploadPath = multerConfig.dest;
+      if (!existsSync(uploadPath)) {
+        mkdirSync(uploadPath);
+      }
+      cb(null, uploadPath);
+    },
+    filename: (req: any, file: any, cb: any) => {
+      newFileName = `${uuid()}${extname(file.originalname)}`;
+      cb(null, newFileName);
+    },
+  }),
+};
+
+@Controller('books')
+export class BooksController {
+  constructor(
+    private readonly booksService: BooksService,
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @Get()
+  async findAll(@Request() req): Promise<BookIdDto[]> {
+    return (await this.booksService.findAll()) as BookIdDto[];
+  }
+
+  @Get(':id')
+  async findOneById(@Param() params): Promise<BookIdDto> {
+    return await this.booksService.findById(params.id);
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file', multerOptions))
+  async create(
+    @Request() req: any,
+    @Body() book: BookDto,
+    @UploadedFile() file,
+  ): Promise<BookDto | any> {
+    const header = req.headers.authorization;
+
+    console.log(book);
+    if (!header) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Not Authorized User',
+      };
+    }
+
+    const token = header.split(' ')[1];
+
+    const authorizedUser = await this.authService.decodeJwt(token);
+
+    if (authorizedUser?.email) {
+      const user = await await this.usersService.findByEmail(
+        authorizedUser.email,
+      );
+
+      if (user) {
+        book.img_url = newFileName;
+        console.log(book);
+        return (await this.booksService.insert(book)) as BookDto;
+      } else {
+        return {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'User Not Authorized to add Book',
+        };
+      }
+    } else {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Not Authorized',
+      };
+    }
+  }
+
+  @Put(':id')
+  @UseInterceptors(FileInterceptor('file', multerOptions))
+  async update(
+    @Request() req: any,
+    @Body() updatedBook: BookDto,
+    @Param() params,
+    @UploadedFile() file,
+  ): Promise<BookIdDto | any> {
+    const header = req.headers.authorization;
+
+    if (!header) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Not Authorized',
+      };
+    }
+
+    const token = header.split(' ')[1];
+
+    const authorizedUser = await this.authService.decodeJwt(token);
+
+    if (authorizedUser?.email) {
+      const user = await await this.usersService.findByEmail(
+        authorizedUser.email,
+      );
+
+      if (user) {
+        const oldBook = await this.booksService.findById(params.id);
+        updatedBook.img_url = newFileName ? newFileName : oldBook.img_url;
+        return await this.booksService.update(oldBook, updatedBook);
+      } else {
+        return {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'User Not Authorized to add Book',
+        };
+      }
+    } else {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Not Authorized',
+      };
+    }
+  }
+
+  @Delete(':id')
+  async delete(@Param() params, @Request() req: any) {
+    const header = req.headers.authorization;
+
+    if (!header) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Not Authorized',
+      };
+    }
+
+    const token = header.split(' ')[1];
+
+    const authorizedUser = await this.authService.decodeJwt(token);
+
+    if (authorizedUser?.email) {
+      const user = await await this.usersService.findByEmail(
+        authorizedUser.email,
+      );
+
+      if (user) {
+        return await this.booksService.delete(params.id);
+      } else {
+        return {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'User Not Authorized to add Book',
+        };
+      }
+    } else {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Not Authorized',
+      };
+    }
+  }
+}
---
+++ Server/src/books/books.entity.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Books {
+  @PrimaryGeneratedColumn()
+  id: string;
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  title: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  genre: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  description: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  author: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  publisher: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  pages: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  price: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  img_url: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  year: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  status: string;
+}
